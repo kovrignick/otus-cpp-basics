4145fe6238d678bb5bff8b6870172a96aee56ab3
Nikolay Kovrigin <nikolay.kovrigin@gs-labs.ru>
2023-04-03 17:47:12 +0300

    add a sample Python script

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a sample Python script"""
+
+
+def print_hi(name='PyCharm'):
+    """Print Hi"""
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi()
