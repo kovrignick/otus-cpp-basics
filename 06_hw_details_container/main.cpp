83231069cffe47f073ed39053a34369e0f7e8b07
nikolay.kovrigin <kovrigin.nickolay@gmail.com>
2023-12-29 12:09:04 +0300

    move checking to another function

--- 06_hw_details_container/main.cpp
+++ 06_hw_details_container/main.cpp
@@ -2,38 +2,43 @@
 #include "SequentialContainer.hpp"
 #include "LinkListContainer.hpp"
 
+template <typename T>
+void start(T container) {
+    // 2
+    for (size_t i = 0; i < 10; ++i) {
+        container.push_back(i);
+    }
+    // 3
+    std::cout << container << std::endl;
+    // 4
+    std::cout << "size: " << container.size() << std::endl;
+    // 5
+    container.erase(6);
+    container.erase(4);
+    container.erase(2);
+    // 6
+    std::cout << container << std::endl;
+    // 7
+    container.insert(0, 10);
+    // 8
+    std::cout << container << std::endl;
+    // 9
+    container.insert(4, 20);
+    // 10
+    std::cout << container << std::endl;
+    // 11
+    container.push_back(30);
+    // 12
+    std::cout << container << std::endl;
+}
+
 int main() 
 {
     // Sequential container
     std::cout << "Sequential container: " << std::endl;
     // 1
     MySequentialContainer<int> seq_int;
-    // 2
-    for (size_t i = 0; i < 10; ++i) {
-        seq_int.push_back(i);
-    }
-    // 3
-    std::cout << seq_int << std::endl;
-    // 4
-    std::cout << "size: " << seq_int.size() << std::endl;
-    // 5
-    seq_int.erase(6);
-    seq_int.erase(4);
-    seq_int.erase(2);
-    // 6
-    std::cout << seq_int << std::endl;
-    // 7
-    seq_int.insert(0, 10);
-    // 8
-    std::cout << seq_int << std::endl;
-    // 9
-    seq_int.insert(4, 20);
-    // 10
-    std::cout << seq_int << std::endl;
-    // 11
-    seq_int.push_back(30);
-    // 12
-    std::cout << seq_int << std::endl;
+    start(seq_int);
     
     std::cout << std::endl;
     
@@ -41,32 +46,7 @@ int main()
     std::cout << "Unidirectional LinkList container: " << std::endl;
     // 1
     MyLinkListContainer1<int> linklist1_int;
-    // 2
-    for (size_t i = 0; i < 10; ++i) {
-        linklist1_int.push_back(i);
-    }
-    // 3
-    std::cout << linklist1_int << std::endl;
-    // 4
-    std::cout << "size: " << linklist1_int.size() << std::endl;
-    // 5
-    linklist1_int.erase(6);
-    linklist1_int.erase(4);
-    linklist1_int.erase(2);
-    // 6
-    std::cout << linklist1_int << std::endl;
-    // 7
-    linklist1_int.insert(0, 10);
-    // 8
-    std::cout << linklist1_int << std::endl;
-    // 9
-    linklist1_int.insert(4, 20);
-    // 10
-    std::cout << linklist1_int << std::endl;
-    // 11
-    linklist1_int.push_back(30);
-    // 12
-    std::cout << linklist1_int << std::endl;
+    start(linklist1_int);
 
 	return 0;
 }
