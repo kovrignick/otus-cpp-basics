6a14f8ee7348919598fd1d7d1160c4b431f1655d
nikolay.kovrigin <kovrigin.nickolay@gmail.com>
2024-02-18 02:19:53 +0300

    with std::lock_guard lock(mtx)

--- 08_hw_topk-words_threads/topk_words.cpp
+++ 08_hw_topk-words_threads/topk_words.cpp
@@ -10,6 +10,8 @@
 #include <map>
 #include <vector>
 #include <chrono>
+#include <mutex>
+#include <thread>
 
 const size_t TOPK = 10;
 
@@ -17,7 +19,7 @@ using Counter = std::map<std::string, std::size_t>;
 
 std::string tolower(const std::string &str);
 
-void count_words(std::istream& stream, Counter&);
+void count_words(std::istream& stream, Counter&, std::mutex &mtx);
 
 void print_topk(std::ostream& stream, const Counter&, const size_t k);
 
@@ -29,13 +31,25 @@ int main(int argc, char *argv[]) {
 
     auto start = std::chrono::high_resolution_clock::now();
     Counter freq_dict;
+    std::vector<std::thread> threads;
+    std::vector<std::ifstream> files;
     for (int i = 1; i < argc; ++i) {
-        std::ifstream input{argv[i]};
-        if (!input.is_open()) {
+        files.emplace_back(argv[i]);
+        if (!files[i-1].is_open()) {
             std::cerr << "Failed to open file " << argv[i] << '\n';
             return EXIT_FAILURE;
         }
-        count_words(input, freq_dict);
+    }
+    std::mutex mtx;
+    for (auto& input: files) {
+        std::thread count_th{count_words, std::ref(input), std::ref(freq_dict), std::ref(mtx)};
+        std::cout << "count_th " << count_th.get_id() << " started working\n";
+        threads.emplace_back(std::move(count_th));
+    }
+    for (auto &count_th: threads) {
+        std::thread::id clerk_id = count_th.get_id();
+        count_th.join();
+        std::cout << "count_th " << clerk_id << " ended his work\n";
     }
 
     print_topk(std::cout, freq_dict, TOPK);
@@ -52,7 +66,8 @@ std::string tolower(const std::string &str) {
     return lower_str;
 };
 
-void count_words(std::istream& stream, Counter& counter) {
+void count_words(std::istream& stream, Counter& counter, std::mutex &mtx) {
+    const std::lock_guard lock(mtx);
     std::for_each(std::istream_iterator<std::string>(stream),
                   std::istream_iterator<std::string>(),
                   [&counter](const std::string &s) { ++counter[tolower(s)]; });    
