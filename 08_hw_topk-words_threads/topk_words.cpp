140397cbaa83e3ac0057da70678056d2f8639e61
Nikolay Kovrigin <nikolay.kovrigin@gs-labs.ru>
2024-02-19 16:30:44 +0300

    solution without mutex

--- 08_hw_topk-words_threads/topk_words.cpp
+++ 08_hw_topk-words_threads/topk_words.cpp
@@ -17,11 +17,15 @@ const size_t TOPK = 10;
 
 using Counter = std::map<std::string, std::size_t>;
 
+const Counter operator+ (const Counter& lhs, const Counter& rhs) {
+    auto result = lhs;
+    for (auto& [string, size]: rhs) result[string] += size;
+    return result;
+}
+
 std::string tolower(const std::string &str);
 
-void count_words(std::istream& stream, Counter&, std::mutex &mtx);
-
-void increase_counter(const std::string &s, Counter& counter, std::mutex &mtx);
+void count_words(std::istream& stream, Counter&);
 
 void print_topk(std::ostream& stream, const Counter&, const size_t k);
 
@@ -32,8 +36,6 @@ int main(int argc, char *argv[]) {
     }
 
     auto start = std::chrono::high_resolution_clock::now();
-    Counter freq_dict;
-    std::vector<std::thread> threads;
     std::vector<std::ifstream> files;
     for (int i = 1; i < argc; ++i) {
         files.emplace_back(argv[i]);
@@ -42,18 +44,24 @@ int main(int argc, char *argv[]) {
             return EXIT_FAILURE;
         }
     }
-    std::mutex mtx;
+    std::vector<std::thread> threads;
+    std::vector<Counter> counters{files.size()};
+    size_t i = 0;
     for (auto& input: files) {
-        std::thread count_th{count_words, std::ref(input), std::ref(freq_dict), std::ref(mtx)};
+        std::thread count_th{count_words, std::ref(input), std::ref(counters[i])};
         std::cout << "count_th " << count_th.get_id() << " started working\n";
         threads.emplace_back(std::move(count_th));
+        ++i;
     }
     for (auto &count_th: threads) {
         std::thread::id clerk_id = count_th.get_id();
         count_th.join();
         std::cout << "count_th " << clerk_id << " ended his work\n";
     }
-
+    Counter freq_dict;
+    for (auto& counter : counters) { 
+        freq_dict = freq_dict + counter; 
+    }
     print_topk(std::cout, freq_dict, TOPK);
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
@@ -68,17 +76,10 @@ std::string tolower(const std::string &str) {
     return lower_str;
 };
 
-void count_words(std::istream& stream, Counter& counter, std::mutex &mtx) {
+void count_words(std::istream& stream, Counter& counter) {
     std::for_each(std::istream_iterator<std::string>(stream),
                   std::istream_iterator<std::string>(),
-                  [&counter, &mtx](const std::string& s){increase_counter(s, counter, mtx);}); 
-}
-
-void increase_counter(const std::string &s, Counter& counter, std::mutex &mtx) {
-    std::string low_s = tolower(s);
-    mtx.lock();
-    ++counter[low_s];
-    mtx.unlock();
+                  [&counter](const std::string &s) { ++counter[tolower(s)]; });    
 }
 
 void print_topk(std::ostream& stream, const Counter& counter, const size_t k) {
